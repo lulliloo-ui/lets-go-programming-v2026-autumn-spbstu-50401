745c14f5ba1485dfafe72684b5f02d81a7f47855
lulliloo-ui <madieva.tv@edu.spbstu.ru>
2026-09-22 13:17:42 +0500

    [TASK-1] Scan and var-naming

--- tatyana.madieva/task-1/cmd/service/main.go
+++ tatyana.madieva/task-1/cmd/service/main.go
@@ -3,28 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	var s_v1 string
-	fmt.Scan(&s_v1)
 	var value1 int
-	_, err1 := fmt.Sscanf(s_v1, "%d", &value1)
+	_, err1 := fmt.Scan(&value1)
 	if err1 != nil {
 		fmt.Println("Invalid first operand")
 		return
 	}
 
-	var s_v2 string
-	fmt.Scan(&s_v2)
 	var value2 int
-	_, err2 := fmt.Sscanf(s_v2, "%d", &value2)
+	_, err2 := fmt.Scan(&value2)
 	if err2 != nil {
 		fmt.Println("Invalid second operand")
 		return
 	}
 
-	var s_op string
-	fmt.Scan(&s_op)
+	var sOp string
+	_, err3 := fmt.Scan(&sOp)
+	if err3 != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
 	var res int
-	switch s_op {
+	switch sOp {
 	case "+":
 		res = value1 + value2
 	case "-":
